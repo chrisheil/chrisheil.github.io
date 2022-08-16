38cdcb4c9bba18e3704158f09ec0ad8afc460e18
Chris Heil <chris.heil@urbanft.com>
2022-08-16 14:56:30 -0500

    Adding test page

---
+++ src/views/TestView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import FavoriteAnimalList from '@/components/FavoriteAnimalList.vue'
+</script>
+
+<template>
+  <main>
+    <FavoriteAnimalList msg="Passing custom message!" />
+  </main>
+</template>
