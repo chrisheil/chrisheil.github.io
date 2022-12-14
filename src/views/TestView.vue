8c1d8bfaab9b780b92cbe0846b7b391bb9907f0c
Chris Heil <chris.heil@urbanft.com>
2022-12-14 09:33:59 -0600

    hard reset to try experiment

--- src/views/TestView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import FavoriteAnimalList from '@/components/FavoriteAnimalList.vue'
-</script>
-
-<template>
-  <main>
-    <FavoriteAnimalList msg="Passing custom message!" />
-  </main>
-</template>
